57692c4402e56e32e891671ccbbf79d8b13d8f8b
Ruben Soler <ruben.soler@mimacom.com>
2021-09-25 23:05:57 +0200

    :sparkles: BankController.kt

---
+++ src/main/kotlin/com/rsginer/hackathoon/banks/controller/BankController.kt
@@ -0,0 +1,17 @@
+package com.rsginer.hackathoon.banks.controller
+
+import com.rsginer.hackathoon.banks.model.Bank
+import com.rsginer.hackathoon.banks.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/banks")
+class BankController(
+    private val bankService: BankService
+) {
+
+    @GetMapping
+    fun getBanks() : Collection<Bank> = this.bankService.getBanks()
+}
---
+++ src/test/kotlin/com/rsginer/hackathoon/banks/controller/BankControllerTest.kt
@@ -0,0 +1,38 @@
+package com.rsginer.hackathoon.banks.controller
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@SpringBootTest
+@AutoConfigureMockMvc
+internal class BankControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+    
+    @Test
+    fun `should return all banks`() {
+        mockMvc.get("/api/banks")
+            .andDo { print() }
+            .andExpect {
+                status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("$[0].accountNumber") {
+                    value("12345")
+                }
+                jsonPath("$[1].accountNumber") {
+                    value("10101")
+                }
+                jsonPath("$[2].accountNumber") {
+                    value("50505")
+                }
+            }
+    }
+    
+}
